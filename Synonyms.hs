5d94c04134cc4cd232f5c36d65ddab2b437f0c70
Steven Ward <stevenward94@gmail.com>
2016-06-13 22:26:51 -0500

    Update 'Synonyms.hs'

--- Synonyms.hs
+++ Synonyms.hs
@@ -5,7 +5,7 @@
  - Last Change:     2016 June 13
  -}
 
-import Data.Map (Map)
+import qualified Data.Map as DM
 
 type PhoneNumber = String
 type Name = String
@@ -14,8 +14,33 @@ type PhoneBook = [(Name,PhoneNumber)]
 inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
 inPhoneBook name pnum pbook = (name,pnum) `elem` pbook
 
-
 type AssocList k v = [(k,v)]
 
-type IntegerMap v = Map Integer v
-type IntMap = Map Int       -- type parameters can be partially applied like functions
+type IntegerMap v = DM.Map Integer v
+type IntMap = DM.Map Int       -- type parameters can be partially applied like functions
+
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Integer (LockerState, Code)
+
+lockerLookup :: Integer -> LockerMap -> Either String Code
+lockerLookup lockerNum map =
+  case Map.lookup lockerNum map of
+    Nothing -> Left $ "Locker number " ++ show lockerNum ++ " doesn't exist!"
+    Just (state, code) -> if state /= Taken
+                        then Right code
+                        else Left $ "Locker number " ++ show lockerNum ++ " is already taken!"
+
+exLockerMap :: LockerMap
+exLockerMap = Map.fromList
+      [(100,(Taken,"ZD39I"))
+      ,(101,(Free,"JAH3I"))
+      ,(103,(Free,"IQSA9"))
+      ,(105,(Free,"QOTSA"))
+      ,(109,(Taken,"893JJ"))
+      ,(110,(Taken,"99292"))
+      ]
