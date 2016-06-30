9881215fba3dea48b5e6353bb291c3c163016be3
Steven Ward <stevenward94@gmail.com>
2016-06-29 21:44:29 -0500

    Update and Reformat 'Learn.IO.hs' to reduce the number of mini-scripts of less than 10 lines of code

--- Learn.IO.hs
+++ Learn.IO.hs
@@ -2,13 +2,26 @@
  - File:          Learn.IO.hs
  - Author:        Steven Ward <stevenward94@gmail.com>
  - URL:           https://github.com/StevenWard94/LearningHaskell
- - Last Change:   2016 June 26
+ - Last Change:   2016 June 29
  -}
+
+-- NOTE: since only one 'main' function can exist, many are renamed to
+-- 'voidmain' (or something similar) to avoid warnings
 
 
 import Data.Char
 
-main = do
+-- this is the "active" 'main' function, which can be edited as needed
+main = interact respondPalindromes
+
+
+respondPalindromes =
+    unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") . lines
+      where isPalindrome xs = xs == reverse xs
+
+
+-- "inactive" 'main' functions
+voidmain = do
     putStrLn "What's your first name?"
     firstName <- getLine
     putStrLn "What's your last name?"
@@ -17,7 +30,7 @@ main = do
         bigLastName  = map toUpper lastName
     putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
 
-main = do
+voidmain' = do
     putStrLn "Hello, what is your name?"
     name <- getLine
     putStrLn ("Hey " ++ name ++ ", you rock!")
