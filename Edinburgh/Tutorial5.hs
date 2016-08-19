986013fc10f3b19379e3060003348376302eb7c2
Steven Ward <stevenward94@gmail.com>
2016-08-19 13:25:31 -0500

    More work on Edinburgh.Tutorial5

--- Edinburgh/Tutorial5.hs
+++ Edinburgh/Tutorial5.hs
@@ -100,6 +100,8 @@ data Prop = Var Name
           | Not Prop
           | Prop :|: Prop
           | Prop :&: Prop
+          | Prop :->: Prop
+          | Prop :<->: Prop
 
 instance Show Prop where
     show  =  showProp
@@ -144,6 +146,8 @@ eval e T              = True
 eval e (Not p)        = not $ eval e p
 eval e (p :|: q)      = eval e p || eval e q
 eval e (p :&: q)      = eval e p && eval e q
+eval e (p :->: q)     = not (eval e p) || eval e q
+eval e (p :<->: q)    = eval e p == eval e q
 
 lookUp :: (Eq a) => a -> [(a,b)] -> b
 lookUp z xys = the [ y | (x,y) <- xys, x == z ]
@@ -159,6 +163,8 @@ showProp T              = "T"
 showProp (Not p)        = "(∼" ++ show p ++ ")"
 showProp (p :|: q)      = "(" ++ show p ++ " | " ++ show q ++ ")"
 showProp (p :&: q)      = "(" ++ show p ++ " & " ++ show q ++ ")"
+showProp (p :->: q)     = "(" ++ show p ++ " → " ++ show q ++ ")"
+showProp (p :<->: q)    = "(" ++ show p ++ " ↔ " ++ show q ++ ")"
 
 -- names: returns all variable names contained in a proposition
 names :: Prop -> Names
@@ -168,6 +174,8 @@ names T              = []
 names (Not p)        = names p
 names (p :|: q)      = nub $ names p ++ names q
 names (p :&: q)      = nub $ names p ++ names q
+names (p :->: q)     = nub $ names p ++ names q
+names (p :<->: q)    = nub $ names p ++ names q
 
 -- envs:  generates a list of all possible truth assignments for the given list
 --      of variables. For example:
@@ -198,3 +206,125 @@ showTable :: [[String]] -> IO ()
 showTable tab  =
     let widths = map length . head $ tab
      in putStrLn $ unlines [ unwords $ zipWith centre widths row | row <- tab ]
+
+tables :: [Prop] -> IO ()
+tables ps  =
+  let xs = nub $ concatMap names ps
+   in showTable $
+       [ xs            ++ [ "|" ] ++ [showProp p | p <- ps]           ] ++
+           [ dashvars xs   ++ [ "|" ] ++ [dash (showProp p) | p <- ps] ] ++
+               [ evalvars e xs ++ [ "|" ] ++ [fort (eval e p) | p <- ps] | e <- envs xs]
+    where dashvars xs       =  [ dash x | x <- xs ]
+          evalvars e xs     =  [ fort $ eval e (Var x) | x <- xs ]
+
+table :: Prop -> IO ()
+table p = tables [p]
+-- \end
+
+-- EXERCISES \begin
+
+-- Exercise 4 \begin
+-- Write the following formulas as 'Props' (call them 'p1', 'p2', and 'p3').
+-- Then use 'satisfiable' to check their satisfiability and 'table' to print
+-- their truth tables.
+
+-- (a) ((P ∨ Q) & (P & Q))
+p1 :: Prop
+p1  =  (Var "P" :|: Var "Q") :&: (Var "P" :&: Var "Q")
+
+p1Satisfiable :: Bool
+p1Satisfiable  =  satisfiable p1
+
+p1Table :: IO ()
+p1Table  =  table p1
+
+
+-- (b) ((P ∨ Q) & ((¬P) & (¬Q)))
+p2 :: Prop
+p2  =  (Var "P" :|: Var "Q") :&: (Not (Var "P") :&: Not (Var "Q"))
+
+p2Satisfiable :: Bool
+p2Satisfiable  =  satisfiable p2
+
+p2Table :: IO ()
+p2Table  =  table p2
+
+
+-- (c) ((P & (Q ∨ R)) & (((¬P) ∨ (¬Q)) & ((¬P) ∨ (¬R))))
+p3 :: Prop
+p3  =  (Var "P" :&: (Var "Q" :|: Var "R")) :&:
+       ((Not (Var "P") :|: Not (Var "Q")) :&: (Not (Var "P") :|: Not (Var "R")))
+
+p3Satisfiable :: Bool
+p3Satisfiable  =  satisfiable p3
+
+p3Table :: IO ()
+p3Table  =  table p3
+
+-- \end
+
+-- Exercise 5 \begin
+
+-- 5. (a) A proposition is a "tautology" if it is always true (i.e. in every
+-- possible environment). Using 'names', 'envs' and 'eval', write a function,
+-- tautology, which checks whether the given proposition is a tautology. Test it
+-- on the examples from Exercise (4) and on their negations.
+tautology :: Prop -> Bool
+tautology = and . ap (map . flip eval) (envs . names)
+
+-- 5. (b) Create two QuickCheck tests to verify that tautology is working
+-- correctly. Use the following facts as the basis for your test properties:
+--     For any property P,
+--       i. either P is a tautology, or ¬P is satisfiable
+--      ii. either P is not satisfiable, or ¬P is not a tautology
+
+prop_tautI :: Prop -> Bool
+prop_tautI  =  liftM2 (||) satisfiable (tautology . Not)
+
+prop_tautII :: Prop -> Bool
+prop_tautII  =  ap ((||) . not . satisfiable) (not . tautology . Not)
+
+prop_taut :: Prop -> Bool
+prop_taut  =  liftM2 (==) tautology (not . satisfiable . Not)
+
+-- \end
+
+-- Exercise 6 \begin
+-- We will extend the datatype and functions for propositions to handle the
+-- connectives, → (implication) and ↔ (bi-implication, or "iff"). They will be
+-- implemented as the constructors, ':->:' and ':<->:'. After you have
+-- implemented them, the truth tables for both should be as follows:
+--
+--   *Main> table (Var "P" :->: Var "Q")        *Main> table (Var "P" :<->: Var "Q")
+--   P Q | (P->Q)                               P Q | (P<->Q)
+--   - - | ------                               - - | -------
+--   F F |   T                                  F F |    T
+--   F T |   T                                  F T |    F
+--   T F |   F                                  T F |    F
+--   T T |   T                                  T T |    T
+
+-- 5. (a) Go to the declaration of the 'Prop' datatype above and extend it with
+-- the infix constructors, ':->:' and ':<->:'
+-- SEE DATA DECLARATION IN INTRODUCTION
+
+-- 5. (b) Extend the definitions for the 'showProp', 'eval' and 'names'
+-- functions to cover the new constructors :->: and :<->:. Test your definitions
+-- by printing out the truth tables above.
+
+-- 5. (c) Define the following formulas as 'Prop's, check their
+-- satisfiability and print their truth tables.
+
+-- (c)   i.  ((P → Q) & (P ↔ Q))
+p4 :: Prop
+p4  =  (Var "P" :->: Var "Q") :&: (Var "P" :<->: Var "Q")
+
+-- (c)  ii.  ((P → Q) & (P & ¬Q))
+p5 :: Prop
+p5  =  (Var "P" :->: Var "Q") :&: (Var "P" :&: Not (Var "Q"))
+
+-- (c) iii.  ((P ↔ Q) & ((P & ¬Q) ∨ (¬P & Q)))
+p6 :: Prop
+p6  =  (Var "P" :<->: Var "Q") :&: ((Var "P" :&: Not (Var "Q")) :|: (Not (Var "P") :&: Var "Q"))
+
+truthTables_p4p5p6 :: IO ()
+truthTables_p4p5p6 = tables [p4,p5,p6]
